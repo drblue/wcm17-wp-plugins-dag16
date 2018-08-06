f7ca360ce7a62c8fb1dbce485e2c43074bf4e335
Johan Nordström <johan@digitalvillage.se>
2018-08-06 14:02:14 +0200

    Final but ugly finished plugin.

--- wcm-calendar/includes/wcm-calendar-ajax.php
+++ wcm-calendar/includes/wcm-calendar-ajax.php
@@ -7,18 +7,32 @@ require_once(dirname(__FILE__) . '/../vendor/autoload.php');
 function ajax_get_events() {
 	$calendarId = $_POST['calendarId'];
 
+	$now = new DateTime();
+	$timeMin = $now->format(DateTime::RFC3339);
+
 	// create Google API Client
 	$client = new Google_Client();
 	$client->setApplicationName("WCM Calendar WordPress Plugin");
 	$client->setDeveloperKey(GOOGLE_API_KEY);
 
 	$service = new Google_Service_Calendar($client);
-	$events = $service->events->listEvents($calendarId);
+	$calendar_events_opts = [
+		'timeMin' => $timeMin,
+		'singleEvents' => true,
+		'orderBy' => 'startTime',
+	];
+	$result = $service->events->listEvents($calendarId, $calendar_events_opts);
 
-	var_dump($events);
-	die();
+	$events = [];
+	foreach ($result->getItems() as $event) {
+		array_push($events, [
+			'summary' => $event->getSummary(),
+			'location' => $event->getLocation(),
+			'start' => $event->getStart()->getDateTime(),
+		]);
+	}
 
-	wp_send_json_success(['calendarId' => $calendarId]);
+	wp_send_json_success(['events' => $events]);
 }
 add_action('wp_ajax_wcm_calendar_get_events', 'ajax_get_events');  // for logged in users
 add_action('wp_ajax_nopriv_wcm_calendar_get_events', 'ajax_get_events'); // for normal users
--- wcm-calendar/public/js/wcm-calendar-public.js
+++ wcm-calendar/public/js/wcm-calendar-public.js
@@ -14,8 +14,21 @@
 					action: 'wcm_calendar_get_events',
 					calendarId: calendarId,
 				},
-				function(data) {
-					console.log("WCM Calendar Widget - Succesfully got events back from Wordpress:", data);
+				function(response) {
+					if (response.success === true) {
+						var events = response.data.events,
+							output = "<ul>";
+
+						events.forEach(function(event) {
+							var event_start = new Date(event.start);
+							var start = event_start.getFullYear() + "-" + (event_start.getMonth()+1) + "-" + event_start.getDate() + " " + event_start.getHours() + ":" + event_start.getMinutes(); // 2018-08-06 09:00
+
+							output += "<li>" + start + " - " + event.summary + " (" + event.location + ")</li>";
+						});
+
+						output += "</ul>";
+						$widget.html(output);
+					}
 				}
 			);
 		});
